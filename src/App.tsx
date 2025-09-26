82e8595ae8c8f5ebf61aa894a42ad890168e7888
Cady Kocanda <cadykocanda@Cadys-MacBook-Pro.local>
2025-09-26 15:06:27 -0500

    welcome

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,10 @@ export default function App() {
   return (
     <div id="app" className="app-root">
       <MenuButton onClick={() => alert('Goals menu clicked!')} />
-      <img src="/images/Home.png" alt="Home" className="home-img" />
+      <div className="home-wrap">
+        <img src="/images/Home.png" alt="Home" className="home-img" />
+        <div className="home-overlay">welcome</div>
+      </div>
     </div>
   )
 }
